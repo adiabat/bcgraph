15c9847f8230c39d06adfa54830bd5f83a14b497
Claire Bao <claireb@mit.edu>
2022-05-03 22:17:51 -0400

    New function that goes through first 100 blocks and prints list of from addresses and to addresses for each transaction

--- txstream.go
+++ txstream.go
@@ -23,12 +23,12 @@ func txStream(txChan chan *wire.MsgTx) {
 		panic(err)
 	}
 
-	info, err := idxFile.Stat()
+	//info, err := idxFile.Stat()
 	if err != nil {
 		panic(err)
 	}
-	maxHeight := info.Size() / 6 // max height based on index file size
-
+	//maxHeight := info.Size() / 6 // max height based on index file size
+	maxHeight := 100
 	curBlockFile := new(os.File)
 	curBlockFileNum := uint16(1 << 15)
 
@@ -36,7 +36,7 @@ func txStream(txChan chan *wire.MsgTx) {
 	var offset uint32
 	var idxChunk [6]byte
 
-	for height := int64(1); height < maxHeight; height++ { // loop through all blocks
+	for height := int64(1); height < int64(maxHeight); height++ { // loop through all blocks
 		_, err = idxFile.ReadAt(idxChunk[:], height*6)
 		if err != nil {
 			panic(err)
@@ -72,6 +72,92 @@ func txStream(txChan chan *wire.MsgTx) {
 	}
 }
 
+func graphGenerate(txChan chan *wire.MsgTx) {
+	m := make(map[string]string)
+	idxFile, err := os.Open(indexFile)
+	if err != nil {
+		panic(err)
+	}
+
+	//info, err := idxFile.Stat()
+	if err != nil {
+		panic(err)
+	}
+	//maxHeight := info.Size() / 6 // max height based on index file size
+	maxHeight := 8000
+
+	curBlockFile := new(os.File)
+	curBlockFileNum := uint16(1 << 15)
+
+	var fileNum uint16
+	var offset uint32
+	var idxChunk [6]byte
+
+	for height := int64(1); height < int64(maxHeight); height++ { // loop through all blocks
+		_, err = idxFile.ReadAt(idxChunk[:], height*6)
+		if err != nil {
+			panic(err)
+		}
+		fileNum = binary.BigEndian.Uint16(idxChunk[0:2])
+		offset = binary.BigEndian.Uint32(idxChunk[2:6])
+
+		if fileNum != curBlockFileNum { // need to switch files
+			if curBlockFile != nil { // close if it's open
+				curBlockFile.Close()
+			}
+			fileName := fmt.Sprintf("blk%05d.dat", fileNum)
+			filePath := filepath.Join(blockDir, fileName)
+			curBlockFile, err = os.Open(filePath)
+			if err != nil {
+				panic(err)
+			}
+		}
+		_, err := curBlockFile.Seek(int64(offset), 0)
+		if err != nil {
+			panic(err)
+		}
+		block := new(wire.MsgBlock)
+		err = block.Deserialize(curBlockFile)
+		if err != nil {
+			panic(err)
+		}
+		// fmt.Printf("%s\n", block.BlockHash().String())
+		for _, tx := range block.Transactions {
+			hash := tx.TxHash().String()
+			fromNodes := make([]string, len(block.Transactions))
+			toNodes := make([]string, len(block.Transactions))
+			for row := 0; row < len(tx.TxIn) || row < len(tx.TxOut); row++ {
+				//fmt.Println(m)
+				if row < len(tx.TxIn) {
+					//index := fmt.Sprintf(",%d",row);
+					//s := hash + index
+					//fmt.Println("looking at in")
+					//fmt.Println(tx.TxIn[row].PreviousOutPoint.String())
+					inAddress, _ := m[tx.TxIn[row].PreviousOutPoint.String()]
+					fromNodes = append(fromNodes, inAddress)
+					//fmt.Printf("%s -> ", tx.TxIn[row].PreviousOutPoint.String())
+				} else {
+					//fmt.Printf("\t\t\t\t\t\t\t\t -> ")
+				}
+				if row < len(tx.TxOut) {
+					//fmt.Printf("%x:%d\n", tx.TxOut[row].PkScript, tx.TxOut[row].Value)
+					index := fmt.Sprintf("%d", row)
+					s := hash + ":" + index
+					address := fmt.Sprintf("%x", tx.TxOut[row].PkScript)
+					//fmt.Println(s)
+					m[s] = address
+					toNodes = append(toNodes, address)
+				} else {
+					//fmt.Printf("\n")
+				}
+			}
+			fmt.Println("edges from ")
+			fmt.Println(fromNodes)
+			fmt.Println(" to ")
+			fmt.Println(toNodes)
+		}
+	}
+}
 func txPrinter(txChan chan *wire.MsgTx) {
 	for {
 		tx := <-txChan
